bffd331ea5b7150b0816b346f745dff7c7ca4677
Mustafa <mustafa05deniz@gmail.com>
2016-02-08 00:54:35 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 var dbconfig = require('../config/database');
 var mysql = require('mysql');
-var connection = mysql.createConnection(dbconfig.connection);
+var connection = mysql.createConnection(dbconfig.connection); // mysql bağlantısını yapıyoruz . 
 
 
 module.exports = function(app) {
@@ -11,13 +11,13 @@ module.exports = function(app) {
             console.log(err);
         } else {
             if (rows.length) {
-                for (var i = 0, len = rows.length; i < len; i++) {
+                for (var i = 0, len = rows.length; i < len; i++) {  //query den gelen bütün parametreleri rows sınıfına ekliyoruz .
                     row[i] = rows[i];
                     
                 }  
             }
         }
-        res.render('index.ejs', {rows : row });
+        res.render('index.ejs', {rows : row }); // index.ejs ye gönderiyoruz . 
     })
     });
 
@@ -31,7 +31,7 @@ module.exports = function(app) {
         soyadi = top.soyad;
         kuladi = top.kuladi;
         
-
+// adi, soyadini, kullaniciadını alıp ekliyor 
        connection.query('insert into kullanicilar(adi,soyad,kuladi) value("'+adi+'","'+soyadi+'","'+kuladi+'") ',function(err,rows){
         if(!err){
             console.log(err);
@@ -51,7 +51,7 @@ module.exports = function(app) {
     app.post('/sil',function(req,res){
         body = req.body;
         console.log(body);
-        console.log(body.data);
+        console.log(body.data); // ekleme işlemindeki işlemin tersi olarak siliyor . 
         connection.query('delete from kullanicilar where id = ?',[body.data])
         res.redirect('/');
     })
