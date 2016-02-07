68ba6219a8150ac288fb451d7284c546fc7231d0
Mustafa <mustafa05deniz@gmail.com>
2016-02-08 00:53:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var dbconfig = require('./config/database');
 var mysql = require('mysql');
-var connection = mysql.createConnection(dbconfig.connection);
+var connection = mysql.createConnection(dbconfig.connection); // mysql bağlantısını yapıyoruz .
 
 
 var express  = require('express');
@@ -10,15 +10,15 @@ var bodyParser = require('body-parser');
 var app      = express();
 var port     = process.env.PORT || 80;
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());  // body kısmından gelen parametreleri almak için kullanıyoruz . 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/views')); // static dosyalarının saklandığı yeri belirliyoruz . 
 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');  // express in template masterini ejs seçiyoruz aynı şekilde hbs veya jade de seçebiliriz 
 
-require('./app/routes.js')(app); 
+require('./app/routes.js')(app);  // routes.js deki fonksiyonları çağırıyoruz .
 
 app.listen(port);
 console.log('Dinleniyor :  ' + port);
